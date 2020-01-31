f7ea5ae63eb0fdc3ee11bd5501a85647bf24c018
pf90 <paidifitz2@gmail.com>
2020-01-31 14:08:58 +0000

    Add billy for shortening urls
    - Add bitly Gem
    - Add before save callback to populate short_url field
    - Update views to display short url instead of original

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,12 +4,16 @@ class Member < ApplicationRecord
   has_many :matches
   has_many :matched_members, through: :matches, dependent: :destroy
 
+  before_save :set_short_url, if: :original_url_changed?
+
   validates :name, :original_url, presence: true
   validates :original_url, url: true
 
   def search_data
-  {
-    name: name,
-  }
-end
+    { name: name }
+  end
+
+  def set_short_url
+    self.short_url = Bitly.client.shorten(original_url).short_url
+  end
 end
---
+++ config/initializers/bitly.rb
@@ -0,0 +1,6 @@
+Bitly.use_api_version_3
+
+Bitly.configure do |config|
+  config.api_version = 3
+  config.access_token = "6d26903dfb3159ff4aa1238dec3557bb2752057d"
+end
